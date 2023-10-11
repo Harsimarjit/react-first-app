150150e3ca7d353c0a3d0ba45a0b85fda8a4dc4c
hbadding <301230749@cencol.ca>
2023-10-11 15:49:42 -0400

    added components

--- react-car-list/src/App.jsx
+++ react-car-list/src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
-// import './App.css'
+import Footer from "./components/Footer"
+import CarCards from "./components/CarsCards"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <nav className='text-2xl flex justify-around 
     bg-gray-300 p-6'>
-      <a href="/html/">HTML</a> 
-      <a href="/css/">CSS</a>
-      <a href="/js/">JavaScript</a> 
-      <a href="/python/">Python</a>
+      <a href="/html/">Carzoona</a> 
+      <a href="/css/">Get a car</a>
+      <a href="/js/">About</a> 
+      <a href="/python/">Login</a>
     </nav>
-    <div className='bg-red-100 h-64 lg:126 my-4'>
-      Learn more...
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 m-8">
+      <CarCards/>
+      <CarCards/>
+      <CarCards/>
+      <CarCards/>
+      <CarCards/>
     </div>
-    
+    <Footer/>
 
     </>
   )
---
+++ react-car-list/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+
+function Footer() {
+
+  return (
+    <>
+        <nav className="flex justify-around text-sm
+        bg-gray-100 py-12">
+            <div className="hover:bg-gray-300 px-4 py-2 rounded">Login</div>
+            <div className="hover:bg-gray-300 px-4 py-2 rounded">Sign up</div>
+            <div className="hover:bg-gray-300 px-4 py-2 rounded">Contact</div>
+            <div className="hover:bg-gray-300 px-4 py-2 rounded">Terms & Conditions</div>
+            <div className="hover:bg-gray-300 px-4 py-2 rounded">About</div>
+        </nav>
+    </>
+  )
+}
+
+export default Footer
